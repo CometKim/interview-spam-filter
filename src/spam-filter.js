a24557ef6f9e9fe66b92d502140307d089c7f6de
Hyeseong Kim <cometkim.kr@gmail.com>
2017-11-05 15:29:57 +0900

    Implement spec 1

--- src/spam-filter.js
+++ src/spam-filter.js
@@ -1,5 +1,17 @@
 // @flow
 
+import XRegExp from 'xregexp';
+
+const linkRegExp = XRegExp.cache('(?i)https?://(?<domain>[^/?# ]+)([/?#]?\w+)?', 'g');
+
+const extractAllLinks = (content: string) => {
+    return XRegExp.match(content, linkRegExp);
+};
+
 export const isSpam = async (content: string, spamLinkDomains: string[], redirectionDepth: number) => {
-    return false;
+    const links = extractAllLinks(content);
+
+    return links
+        .map(link => XRegExp.exec(link, linkRegExp).domain)
+        .some(domain => spamLinkDomains.includes(domain));
 };
